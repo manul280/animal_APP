b840f99e6bddccf095390a7f6231bd9b6f577368
manul280 <nekokan1192@gmail.com>
2023-04-18 02:27:45 +0900

    app.py修正

--- src/animal.py
+++ src/animal.py
@@ -8,6 +8,8 @@ from torchvision.models import resnet18
 
 # 学習済みモデルに合わせた前処理を追加
 transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
